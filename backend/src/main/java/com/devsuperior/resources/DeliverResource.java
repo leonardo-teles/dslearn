d36fb726f43ce4d506dde783d15f953220148590
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-08-02 01:52:29 -0300

    Pré-autorizando Métodos por Perfil de Usuário

---
+++ backend/src/main/java/com/devsuperior/resources/DeliverResource.java
@@ -0,0 +1,27 @@
+package com.devsuperior.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.dto.DeliverRevisionDTO;
+import com.devsuperior.services.DeliverService;
+
+@RestController
+@RequestMapping(value = "/deliveries")
+public class DeliverResource {
+
+	@Autowired
+	private DeliverService service;
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Void> saveRevision(@PathVariable Long id, @RequestBody DeliverRevisionDTO dto) {
+		service.saveRevision(id, dto);
+		
+		return ResponseEntity.noContent().build();	
+	}
+}
---
+++ backend/src/main/java/com/devsuperior/services/DeliverService.java
@@ -0,0 +1,28 @@
+package com.devsuperior.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dto.DeliverRevisionDTO;
+import com.devsuperior.entities.Deliver;
+import com.devsuperior.repositories.DeliverRepository;
+
+@Service
+public class DeliverService {
+
+	@Autowired
+	private DeliverRepository repository;
+	
+	@Transactional
+	@PreAuthorize("hasAnyRole('ADMIN','INSTRUCTOR')")
+	public void saveRevision(Long id, DeliverRevisionDTO dto) {
+		Deliver deliver = repository.getOne(id);
+		deliver.setStatus(dto.getStatus());
+		deliver.setFeedback(dto.getFeedback());
+		deliver.setCorrectCount(dto.getCorrectCount());
+		
+		repository.save(deliver);
+	}
+}
