03cffea82947e381a587d49ce0a1c514c6d803e5
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-08-01 23:51:33 -0300

    Autorização Customizada a Nível de Serviço - Parte 2

--- backend/src/main/java/com/devsuperior/entities/User.java
+++ backend/src/main/java/com/devsuperior/entities/User.java
@@ -150,6 +150,15 @@ public class User implements UserDetails, Serializable {
 		return true;
 	}
 	
+	public boolean hasRole(String roleName) {
+		for(Role role : roles) {
+			if(role.getAuthority().equals(roleName)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
